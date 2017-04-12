0acc41cc3d0fa5ad9a2d32b80a0f6cce7d5b531c
mapvex <mapvex@mail.com>
2017-04-12 22:11:31 +0300

    Create Point.java

---
+++ src/bot/simulation/objects/Point.java
@@ -0,0 +1,15 @@
+package bot.simulation.objects;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getLength() {
+        return Math.sqrt(x * x + y * y);
+    }
+}
