afe4d21058adcf760b2248fa45a540dbf005fef0
mapvex <mapvex@mail.com>
2017-04-12 22:06:04 +0300

    Create CollisionResult.java

---
+++ src/bot/simulation/collision/CollisionResult.java
@@ -0,0 +1,11 @@
+package bot.simulation.collision;
+
+public class CollisionResult {
+    public double time;
+    public double _SafeStr_1744;
+
+    public CollisionResult(double _arg_1, double _arg_2) {
+        this.time = _arg_1;
+        this._SafeStr_1744 = _arg_2;
+    }
+}
